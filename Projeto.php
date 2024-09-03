c9d9515a47aeb283b841acddd32f1c0feaaaddb6
Enrique-Feliczaki <enrique.zwetsch@gmail.com>
2024-09-03 08:38:15 -0300

    Configuração incial e Funções auxiliares

--- Projeto.php
+++ Projeto.php
@@ -1,1 +1,18 @@
 <?php
+$usuarios = [
+    ["login" => "admin", "senha" => "12345"]
+];
+
+$log = [];
+$totalVendas = 0;
+
+function limparTela()
+{
+    system('clear');
+}
+
+function registrarLog($mesagem)
+{
+    global $log;
+    $log[] = $mesagem . "em" . date("d/m/Y H:i:s");
+}
